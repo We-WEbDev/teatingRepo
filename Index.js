f14f8d36a32d0a802870d017aa81825650cdceb3
Parl582 <pranjalgogoi582@gmail.com>
2022-07-08 13:53:13 +0530

    test

---
+++ Index.js
@@ -0,0 +1,45 @@
+
+//TODO: callbackFunction ]
+
+// ===> When we passed a function into a function ad an argument is known as callback function 🕦 
+
+// function x(c) {
+//     c()
+// }
+// x(name)
+// function name() {
+//     console.log("i Am CallBack")
+// }
+
+// another example of callback
+
+// function greeting(name) {
+//     alert('Hello ' + name);
+//   }
+  
+//   function processUserInput(callback) {
+//     var Yourname = prompt('Please enter your name.');
+//     callback(Yourname);
+//   }
+  
+//   processUserInput(greeting);
+
+
+// var name = 'Pranjal'
+// geetingSomeOne(name)
+
+// if we want it dynamic we need to write a function 
+
+// function EnterYourName (passName){
+
+//   var YourName =  prompt("Enter Your Name")
+//   passName(YourName)
+
+// }
+// EnterYourName(geetingSomeOne)
+
+// function geetingSomeOne (name){
+//     console.log('hello' + " " + name)
+// }
+
+
